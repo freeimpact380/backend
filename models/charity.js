bd7da255cea2286211f8e78336b7547fa713df42
Jahaan Jain <jahaanjain@gmail.com>
2021-12-02 10:47:16 -0500

    changed charity model

--- models/charity.js
+++ models/charity.js
@@ -4,6 +4,7 @@ var mongoose = require("mongoose"),
 var CharitySchema = new Schema({
     charityName: { type: String, required: true, index: { unique: true } },
     totalDonated: { type: Number, default: 0 },
+    profilePicture: { type: String, default: "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y" },
 });
 
 module.exports = mongoose.model("Charity", CharitySchema);
